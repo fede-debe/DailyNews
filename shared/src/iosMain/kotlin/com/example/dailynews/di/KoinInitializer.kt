7bf914fda71e6e4fbebbf4cc8e24f6206a71ec23
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-21 10:30:27 +0200

    Feat: Initialize Koin for Dependency Injection in iOS
    This commit introduces Koin for dependency injection in the iOS application and updates the ArticlesScreen to use the injected ViewModel.
    
    The following changes were made:
    - Added `shared/src/iosMain/kotlin/com/example/dailynews/di/KoinInitializer.kt`:
        - Defined an `initKoin()` function to initialize Koin with shared modules.
        - Created an `ArticlesInjector` class, which is a `KoinComponent`, to inject `ArticlesViewModel`.
    - Modified `iosApp/iosApp/iOSApp.swift`:
        - Called `KoinInitializerKt.doInitKoin()` in the `init()` method to initialize Koin when the app starts.
    - Modified `iosApp/iosApp/Screens/ArticlesScreen.swift`:
        - Changed the initialization of `articlesViewModel` to use `ArticlesInjector().articlesViewModel` to get the ViewModel instance from Koin.

---
+++ shared/src/iosMain/kotlin/com/example/dailynews/di/KoinInitializer.kt
@@ -0,0 +1,19 @@
+package com.example.dailynews.di
+
+import com.example.dailynews.articles.ui.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+fun initKoin() {
+    /** Dependency graph */
+    val modules = sharedKoinModules
+
+    startKoin {
+        modules(modules)
+    }
+}
+
+class ArticlesInjector : KoinComponent {
+    val articlesViewModel: ArticlesViewModel by inject()
+}
