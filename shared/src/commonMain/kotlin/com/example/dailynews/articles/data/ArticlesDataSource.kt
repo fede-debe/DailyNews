7acafeabcd426b6ee58e0ba7f3f26ac5273172bb
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-22 18:42:36 +0200

    Feat: Implement ArticlesRepository for data fetching and caching
    This commit introduces the `ArticlesRepository` class to handle the fetching and caching of articles.
    
    The following changes were made:
    - Created `ArticlesRepository.kt`:
        - This class manages article data, interacting with `ArticlesDataSource` for local storage and `ArticlesService` for network requests.
        - The `getArticles` function now optionally fetches fresh data or returns cached articles.
        - Added a `fetchArticles` private function to retrieve articles from the service and store them in the database.
    - Modified `ArticlesUseCase.kt`:
        - Updated to use `ArticlesRepository` instead of directly calling `ArticlesService`.
        - The `getArticles` function now accepts a `forceFetch` parameter to control whether to refresh data from the network.

---
+++ shared/src/commonMain/kotlin/com/example/dailynews/articles/data/ArticlesDataSource.kt
@@ -0,0 +1,51 @@
+package com.example.dailynews.articles.data
+
+import com.example.dailynews.articles.domain.ArticleRaw
+import com.example.dailynews.db.DailyNewsDatabase
+
+class ArticlesDataSource(private val database: DailyNewsDatabase) {
+
+    fun getAllArticles(): List<ArticleRaw> =
+        database.dailyNewsDatabaseQueries.selectAllArticles(::mapToArticleRaw).executeAsList()
+
+    /**
+      A transaction is a block of queries that must all be successful.
+      If even one of the queries fails, the entire transaction and all of
+      its preceding queries must be reverted.
+
+      Essentially, all queries inside the transaction block must execute
+      successfully as a single, atomic unit.
+    */
+    fun insertArticles(articles: List<ArticleRaw>) {
+        database.dailyNewsDatabaseQueries.transaction {
+            articles.forEach { articleRaw ->
+                insertArticle(articleRaw)
+            }
+        }
+    }
+
+    fun clearArticles() =
+        database.dailyNewsDatabaseQueries.removeAllArticles()
+
+    private fun insertArticle(articleRaw: ArticleRaw) {
+        database.dailyNewsDatabaseQueries.insertArticle(
+            articleRaw.title,
+            articleRaw.desc,
+            articleRaw.date,
+            articleRaw.imageUrl
+        )
+    }
+
+    private fun mapToArticleRaw(
+        title: String,
+        desc: String?,
+        date: String,
+        url: String?
+    ): ArticleRaw =
+        ArticleRaw(
+            title,
+            desc,
+            date,
+            url
+        )
+}
