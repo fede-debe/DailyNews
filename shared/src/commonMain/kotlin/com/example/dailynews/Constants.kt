5ae932100720506f4fc9aa42b41558f950355be1
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-24 17:49:18 +0200

    Refactor: Centralize API key in Constants
    This commit centralizes the News API key into a `Constants` object to improve maintainability and avoid hardcoding it in multiple places.
    
    The following changes were made:
    - Added `NEWS_API_KEY` constant to `shared/src/commonMain/kotlin/com/example/dailynews/Constants.kt`.
    - Updated `ArticleService.kt` to use `Constants.NEWS_API_KEY` instead of a hardcoded API key.
    - Updated `SourcesService.kt` to use `Constants.NEWS_API_KEY` instead of a hardcoded API key.

--- shared/src/commonMain/kotlin/com/example/dailynews/Constants.kt
+++ shared/src/commonMain/kotlin/com/example/dailynews/Constants.kt
@@ -2,5 +2,6 @@ package com.example.dailynews
 
 object Constants {
     const val DEFAULT_IMAGE_URL = "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
+    const val NEWS_API_KEY = "6cf02f77b49a420597917c6c35f0fddb"
 
 }
--- shared/src/commonMain/kotlin/com/example/dailynews/articles/data/ArticleService.kt
+++ shared/src/commonMain/kotlin/com/example/dailynews/articles/data/ArticleService.kt
@@ -1,6 +1,7 @@
 package com.example.dailynews.articles.data
 
 
+import com.example.dailynews.Constants
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -9,10 +10,11 @@ class ArticlesService(private val httpClient: HttpClient) {
 
     private val country = "us"
     private val category = "business"
-    private val apiKey = "6cf02f77b49a420597917c6c35f0fddb"
 
     suspend fun fetchArticles(): List<ArticleRaw> {
-        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey").body()
+        val response: ArticlesResponse =
+            httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=${Constants.NEWS_API_KEY}")
+                .body()
         return response.articles
     }
 }
