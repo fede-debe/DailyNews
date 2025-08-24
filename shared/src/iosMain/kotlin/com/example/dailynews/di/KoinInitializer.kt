65af4eab346727708dffad0eab7615820ba2eae1
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-24 17:49:39 +0200

    Feat: Add SourcesInjector for iOS
    This commit introduces a `SourcesInjector` class in the iOS-specific Koin dependency injection setup.
    
    The following change was made:
    - Added `SourcesInjector` class in `shared/src/iosMain/kotlin/com/example/dailynews/di/KoinInitializer.kt`. This class provides an injected instance of `SourcesViewModel`.

--- shared/src/iosMain/kotlin/com/example/dailynews/di/KoinInitializer.kt
+++ shared/src/iosMain/kotlin/com/example/dailynews/di/KoinInitializer.kt
@@ -1,6 +1,7 @@
 package com.example.dailynews.di
 
 import com.example.dailynews.articles.presentation.ArticlesViewModel
+import com.example.dailynews.sources.presentation.SourcesViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.context.startKoin
@@ -16,4 +17,8 @@ fun initKoin() {
 
 class ArticlesInjector : KoinComponent {
     val articlesViewModel: ArticlesViewModel by inject()
+}
+
+class SourcesInjector : KoinComponent {
+    val sourcesViewModel: SourcesViewModel by inject()
 }
