85f17686a6988c825347618cfa50b309861aee0c
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-24 12:27:25 +0200

    Feat: Add Sources feature components
    This commit introduces the necessary components for the "Sources" feature.
    
    The following changes were made:
    - Added `sourcesModule` to the `sharedKoinModules` in `shared/src/commonMain/kotlin/com/example/dailynews/di/SharedKoinModules.kt` to include source-related dependencies.
    - Registered `SourcesViewModel` in `viewModelsModule` in `composeApp/src/androidMain/kotlin/com/example/dailynews/di/ViewModelModules.kt` for Android.
    - Updated `shared/src/commonMain/sqldelight/com/example/dailynews/db/DailyNewsDatabase.sq` to:
        - Define a new `Source` table with columns: `id`, `name`, `desc`, `language`, and `country`.
        - Add SQL queries:
            - `insertSource`: To insert a new source.
            - `selectAllSources`: To retrieve all sources.
            - `removeAllSources`: To delete all sources.

--- composeApp/src/androidMain/kotlin/com/example/dailynews/di/ViewModelModules.kt
+++ composeApp/src/androidMain/kotlin/com/example/dailynews/di/ViewModelModules.kt
@@ -1,10 +1,12 @@
 package com.example.dailynews.di
 
 import com.example.dailynews.articles.presentation.ArticlesViewModel
+import com.example.dailynews.sources.presentation.SourcesViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelsModule = module {
 
     viewModelOf(::ArticlesViewModel)
+    viewModelOf(::SourcesViewModel)
 }
--- shared/src/commonMain/kotlin/com/example/dailynews/di/SharedKoinModules.kt
+++ shared/src/commonMain/kotlin/com/example/dailynews/di/SharedKoinModules.kt
@@ -1,9 +1,11 @@
 package com.example.dailynews.di
 
 import com.example.dailynews.articles.di.articlesModule
+import com.example.dailynews.sources.di.sourcesModule
 
 /** Modules shared between different platforms */
 val sharedKoinModules = listOf(
     articlesModule,
+    sourcesModule,
     networkModule
 )
