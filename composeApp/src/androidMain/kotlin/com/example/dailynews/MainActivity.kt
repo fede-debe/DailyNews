e8000721aa0367f0b733882013ef27de3aee8822
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-25 13:55:15 +0200

    Refactor: Centralize App Composable for Multiplatform Use
    This commit centralizes the main `App` composable function into the `shared` module for use by both Android and iOS platforms.
    
    The following changes were made:
    - Created `shared/src/commonMain/kotlin/com/example/dailynews/ui/ComposeMutliplatformApp.kt`: This new file defines the `App` composable, which displays a simple "Hello world!" text within a `Scaffold` and `MaterialTheme`.
    - Modified `composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt`: Updated to use the new shared `App` composable, replacing the previous `AppScaffold`.
    - Created `shared/src/iosMain/kotlin/com/example/dailynews/MainIOS.kt`: This new file defines `MainViewController` for iOS, which uses the shared `App` composable as its entry point.

--- composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.dailynews.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppScaffold()
+//                    AppScaffold()
+                    App()
                 }
             }
         }
---
+++ shared/src/iosMain/kotlin/com/example/dailynews/MainIOS.kt
@@ -0,0 +1,11 @@
+package com.example.dailynews
+
+import androidx.compose.ui.window.ComposeUIViewController
+import com.example.dailynews.ui.App
+
+/** MainViewController is like fragment/activity in Android */
+fun MainViewController() = ComposeUIViewController {
+    /** whatever composable function we put inside here,
+     *  is going to be the entry point and what the iOS emulator is going to show. */
+    App()
+}
