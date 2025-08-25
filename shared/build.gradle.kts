d2113688295033ed4365ff7999e433ca3325d0d6
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-25 10:44:27 +0200

    Feat: Integrate Compose Multiplatform for UI development
    This commit adds the necessary dependencies and plugins to enable Compose Multiplatform for building user interfaces across different platforms.
    
    The following changes were made:
    - Added `composeMultiplatform` and `composeCompiler` plugins to `shared/build.gradle.kts`.
    - Included Compose runtime, foundation, components resources, Material3, and Material dependencies in the `commonMain` source set of `shared/build.gradle.kts`.
    - Ensured `composeMultiplatform` and `composeCompiler` plugins are applied in the root `build.gradle.kts`.
    - Added `kotlinx-datetime` and `compose-material` library definitions to `gradle/libs.versions.toml`.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.skie)
     alias(libs.plugins.kotlinPluginSerialization)
     alias(libs.plugins.sqlDelight)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
@@ -38,10 +40,16 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
-
+            implementation(libs.kotlinx.datetime)
             implementation(libs.koin.core)
 
-            implementation(libs.kotlinx.datetime)
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.components.resources)
+            implementation(compose.material3)
+            implementation(libs.compose.material)
+
+
         }
 
         androidMain.dependencies {
