a8eaff5e084fa26b181b650b24648bd63bb02914
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-08-25 10:44:51 +0200

    Refactor: Update Gradle settings
    This commit updates the `settings.gradle.kts` file to simplify repository declarations and reorder the root project name.
    
    The following changes were made:
    - Replaced specific `mavenContent` blocks for Google repositories with the simpler `google()` shortcut.
    - Added `maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")` to both `pluginManagement` and `dependencyResolutionManagement` repositories.
    - Moved `rootProject.name = "DailyNews"` to the end of the file.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,32 +1,22 @@
-rootProject.name = "DailyNews"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
+        google()
         gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
+        google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
+rootProject.name = "DailyNews"
 include(":composeApp")
 include(":shared")
