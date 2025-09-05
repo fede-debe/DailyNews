7283c1e32780b851c61426b6541f137da8546700
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-09-05 16:48:44 +0200

    Refactor: Update App Entry Points for Android and iOS
    This commit updates the entry point composables for both the Android and iOS applications.
    
    The following changes were made:
    - Modified `shared/src/iosMain/kotlin/com/example/dailynews/MainIOS.kt`: Updated `MainViewController` to display `SourcesScreen()` instead of `App()`.
    - Modified `composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt`: Updated `MainActivity` to display `ArticlesScreen()` instead of `App()`.

--- composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/dailynews/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.dailynews.ui.App
+import com.example.dailynews.ui.screens.ArticlesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    AppScaffold()
-                    App()
+                    ArticlesScreen()
                 }
             }
         }
--- shared/src/iosMain/kotlin/com/example/dailynews/MainIOS.kt
+++ shared/src/iosMain/kotlin/com/example/dailynews/MainIOS.kt
@@ -2,10 +2,12 @@ package com.example.dailynews
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.example.dailynews.ui.App
+import com.example.dailynews.ui.screens.AboutScreen
+import com.example.dailynews.ui.screens.SourcesScreen
 
 /** MainViewController is like fragment/activity in Android */
 fun MainViewController() = ComposeUIViewController {
     /** whatever composable function we put inside here,
      *  is going to be the entry point and what the iOS emulator is going to show. */
-    App()
+    SourcesScreen()
 }
